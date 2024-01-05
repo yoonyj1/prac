7e8124db51fe2930d68759128d8a677f3a59b713
sumin <330sum@naver.com>
2024-01-05 11:00:36 +0900

    sumin

---
+++ practice/src/main/java/com/example/practice/sumin/Sumin.java
@@ -0,0 +1,10 @@
+package com.example.practice.sumin;
+
+import org.springframework.boot.SpringApplication;
+
+public class Sumin {
+	
+	public void method1() {
+		System.out.println("hello");
+	}
+}
